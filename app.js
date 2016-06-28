553a80cb7aa5f7d394d043eb3ab4081b3517ffc2
xueyuwan <1213287572@qq.com>
2016-06-28 17:13:50 +0800

    test heroku

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var bodyparser = require('body-parser');
+
+app.use(function(req,res,next){
+   var name = req.params;
+
+  res.json(name);
+});
+app.use(bodyparser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(process.env.PORT ||3001,function(){
+  console.log("server listening ");
+});
+module.exports = app;
